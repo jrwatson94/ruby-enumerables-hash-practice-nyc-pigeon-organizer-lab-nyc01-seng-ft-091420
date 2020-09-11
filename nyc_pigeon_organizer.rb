3ce3fcd4571470d9d8abc0c20040d964b9ca991e
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 00:12:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  names = []
+  names.push(data[:gender][:male] + data[:gender][:female])
+  
+  names.each do |name|
+    new_data= {name => {}}
+  end
+  
+  
+    binding.pry
 end
